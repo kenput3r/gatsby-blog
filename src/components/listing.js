0002e83a175a21f35c9d8766c431af30a384bb00
Kenneth Lyerly <kenput3r@gmail.com>
2019-11-30 14:17:20 -0800

    refactor to use useStaticQuery

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,20 +1,50 @@
 import React from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const Listing = () => (
-  <div>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const LISTING_QUERY = graphql`
+  query BlogPostListing {
+    allMarkdownRemark(limit: 5, sort: {
+      order: DESC,
+      fields: [frontmatter___date]
+    }) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+const Listing = () => {
+  const data = useStaticQuery(LISTING_QUERY);
+  return (
+    <div>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      {data.allMarkdownRemark.edges.map(edge => (
+        <article key={edge.node.frontmatter.slug}>
+          <h2>{edge.node.frontmatter.title}</h2>
+          <p>{edge.node.frontmatter.date}</p>
+          <p>{edge.node.excerpt}</p>
+          <Link to={`/posts${edge.node.frontmatter.slug}`}>Read More</Link>
+        </article>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+  )
+}
 
 export default Listing
